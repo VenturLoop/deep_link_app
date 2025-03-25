c6e6b5f86e007e711d416b3f04cab3e347a0ebc0
Atharva <teteatharva@gmail.com>
2025-03-25 19:11:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Parse JSON requests
+
+// Serve static files from "public"
+app.use(express.static("public"));
+// Routes
+app.get("/", (req, res) => {
+  res.send("🚀 Welcome to Venturloop Backend!");
+});
+
+app.get("/.well-known/assetlinks.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.sendFile(
+    path.join(__dirname, "public", ".well-known", "assetlinks.json")
+  );
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
