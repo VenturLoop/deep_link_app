cd8d4529b16311b454d8c4cec4333a5c92a892cb
Atharva <teteatharva@gmail.com>
2025-04-02 11:17:23 +0530

    Reinitialized repository

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get("/callback", async (req, res) => {
 
     // Send `id_token` to your backend for processing
     const backendResponse = await fetch(
-      `https://venturloopbackend-v-1-0-9.onrender.com/auth/google-signup`,
+      `https://digitalocean.venturloop.com/auth/google-signup`,
       {
         method: "POST",
         headers: {
@@ -77,26 +77,13 @@ app.get("/callback", async (req, res) => {
     }
 
     const backendData = await backendResponse.json();
+
     const appId = backendData.user._id;
-    // Login with google
+
     let deepLink = `venturloop://callback/auth/login?userId=${encodeURIComponent(
       appId
     )}`;
 
-    // Allready account is created with different authType
-    if (
-      backendData.isNewUser === false &&
-      backendData.user.authType &&
-      backendData.user.authType !== "google"
-    ) {
-      deepLink = `venturloop://callback/auth/login?userId=${encodeURIComponent(
-        appId
-      )}&message=${encodeURIComponent(
-        `Account allready exists. Use your ${backendData.user.authType} login.`
-      )}`;
-    }
-
-    // New account created with authType google
     if (backendData.isNewUser) {
       deepLink = `venturloop://callback/auth/signIn?userId=${encodeURIComponent(
         appId
@@ -109,7 +96,6 @@ app.get("/callback", async (req, res) => {
     res.status(500).json({
       error: "Authentication failed",
       details: error.response?.data || error.message,
-      main_Error : error
     });
   }
 });
@@ -141,7 +127,7 @@ app.get("/callback_linkedIn", async (req, res) => {
 
     // Send `id_token` to your backend for processing
     const backendResponse = await fetch(
-      `https://venturloopbackend-v-1-0-9.onrender.com/auth/linkedIn-signup`,
+      `https://digitalocean.venturloop.com/auth/linkedIn-signup`,
       {
         method: "POST",
         headers: {
@@ -165,19 +151,6 @@ app.get("/callback_linkedIn", async (req, res) => {
     let deepLink = `venturloop://callback/auth/login?userId=${encodeURIComponent(
       appId
     )}`;
-
-    // Allready account is created with different authType
-    if (
-      !backendData.isNewUser &&
-      backendData.user.authType &&
-      backendData.user.authType !== "linkedIn"
-    ) {
-      deepLink = `venturloop://callback/auth/login?userId=${encodeURIComponent(
-        appId
-      )}&message=${encodeURIComponent(
-        `Account allready exists. Use your ${backendData.user.authType} login.`
-      )}`;
-    }
 
     if (backendData.isNewUser) {
       deepLink = `venturloop://callback/auth/signIn?userId=${encodeURIComponent(
