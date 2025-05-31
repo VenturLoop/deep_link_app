056e8b98de6d7610204a670a5b6ff01e659ece88
Atharva <teteatharva@gmail.com>
2025-06-01 01:21:10 +0530

    new

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.post("/auth/google", async (req, res) => {
         code,
         client_id: process.env.GOOGLE_CLIENT_ID,
         client_secret: process.env.GOOGLE_CLIENT_SECRET,
-        redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+        redirect_uri: "venturloop://callback",
         grant_type: "authorization_code",
       }).toString(),
       {
